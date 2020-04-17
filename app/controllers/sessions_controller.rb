aefb7dc4f6c11e06d32fe52ac1854ad79a126b0e
YoshidaTatsumi <yoichi_1308@yahoo.co.jp>
2020-04-18 05:33:01 +0900

    [finish]９章

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,8 @@ class SessionsController < ApplicationController
   	user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
     	log_in user
+      remember user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)   #チェックボックスがオンのときに'1'になり、オフのときに'0'になります
       redirect_to user 		#user_url(user))と同じ
     else
     	flash.now[:danger] = 'Invalid email/password combination'
@@ -14,7 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
